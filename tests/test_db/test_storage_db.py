81e1d1f2008daf24044277fca096f973b25904f9
Sebastian Buchner <sebbel9@freenet.de>
2026-02-19 20:39:25 +0100

    added test_delete_storage and test_get_storage to test_storage_db

--- tests/test_db/test_storage_db.py
+++ tests/test_db/test_storage_db.py
@@ -14,3 +14,32 @@ def test_insert_storage(tmp_path):
     )
     result = db.cursor.fetchone()
     assert result[0] == "Keller"
+
+def test_delete_storage(tmp_path):
+    test_db = tmp_path / "test.db"
+    db = StorageDatabase(test_db)
+    db.create_table()
+
+    db.insert_storage("Speicher")
+    db.delete_storage("Speicher")
+
+    db.cursor.execute("""
+    SELECT lagerort FROM lagerort WHERE lagerort=?""",
+    ("Speicher",)
+    )
+    result = db.cursor.fetchone()
+    assert result is  None
+
+def test_get_storage(tmp_path):
+    test_db = tmp_path / "test.db"
+    db = StorageDatabase(test_db)
+    db.create_table()
+
+    db.insert_storage("Abstellraum")
+    db.insert_storage("Gefriertruhe")
+
+    result = db.get_storage()
+
+    assert len(result) == 2
+    assert result[0][0] == "Abstellraum"
+    assert result[1][0] == "Gefriertruhe"
