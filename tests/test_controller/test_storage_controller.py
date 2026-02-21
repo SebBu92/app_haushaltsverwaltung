db01db108cfd8e41985575395d2afdec54065c54
Sebastian Buchner <sebbel9@freenet.de>
2026-02-21 13:27:43 +0100

    added test_save_storage_without_value and test_save_storage_with_default_value to test_storage_controller

--- tests/test_controller/test_storage_controller.py
+++ tests/test_controller/test_storage_controller.py
@@ -11,3 +11,32 @@ def test_get_storage_calls_db():
 
     mock_db.get_storage.assert_called_once()
     assert result == [("Keller",)]
+
+def test_save_storage_without_value():
+    mock_db = Mock()
+    
+    controller = StorageController(mock_db)
+
+    with pytest.raises(ValueError):
+        controller.save_storage("")
+
+    mock_db.insert_storage.assert_not_called()
+
+def test_save_storage_with_default_value():
+    mock_db = Mock()
+
+    controller = StorageController(mock_db)
+
+    with pytest.raises(ValueError):
+        controller.save_storage("Lagerort hinzufügen")
+
+    mock_db.insert_storage.assert_not_called()
+
+def test_save_storage_with_valid_value():
+    pass
+
+def test_delete_storage_without_value():
+    pass
+
+def test_delete_storage_with_valid_value():
+    pass
