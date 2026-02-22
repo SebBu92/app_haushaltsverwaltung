b5d1eca8b39c6b9a2ea0bf7e8f4780f84d12b6fa
Sebastian Buchner <sebbel9@freenet.de>
2026-02-22 18:58:39 +0100

    added test_delete_storage_wit_valid_value to test_storage_controller

--- tests/test_controller/test_storage_controller.py
+++ tests/test_controller/test_storage_controller.py
@@ -4,13 +4,13 @@ from controller.storage_controller import StorageController
 
 def test_get_storage_calls_db():
     mock_db = Mock()
-    mock_db.get_storage.return_value = [("Keller",)]
+    mock_db.get_storage.return_value = "Keller"
 
     controller = StorageController(mock_db)
     result = controller.get_storage()
 
     mock_db.get_storage.assert_called_once()
-    assert result == [("Keller",)]
+    assert result == "Keller"
 
 def test_save_storage_without_value():
     mock_db = Mock()
@@ -33,7 +33,14 @@ def test_save_storage_with_default_value():
     mock_db.insert_storage.assert_not_called()
 
 def test_save_storage_with_valid_value():
-    pass
+    mock_db = Mock()
+    mock_db.insert_storage.return_value = "Keller"
+
+    controller = StorageController(mock_db)
+    result = controller.save_storage("Keller")
+
+    mock_db.insert_storage.assert_called_once_with("Keller")
+    assert result == "Keller"
 
 def test_delete_storage_without_value():
     mock_db = Mock()
@@ -42,7 +49,15 @@ def test_delete_storage_without_value():
 
     with pytest.raises(ValueError):
         controller.delete_storage("")
+
     mock_db.insert_storage.assert_not_called()
 
 def test_delete_storage_with_valid_value():
-    pass
+    mock_db = Mock()
+    mock_db.delete_storage.return_value = None
+
+    controller = StorageController(mock_db)
+    result = controller.delete_storage("Garage")
+
+    mock_db.delete_storage.assert_called_once_with("Garage")
+    assert result is None
