03869c796eea9d6229c34308c9d93b805294a3f0
Sebastian Buchner <sebbel9@freenet.de>
2026-03-02 20:09:34 +0100

    added tests for method filter_supplies_by_entry

--- tests/test_controller/test_supplies_controller.py
+++ tests/test_controller/test_supplies_controller.py
@@ -33,7 +33,7 @@ def test_sort_by_mhd_desc():
     mock_db.sort_mhd_desc.assert_called_once_with()
     mock_db.get_supplies.assert_not_called()
 
-def test_sort_by_mhd_with_value():
+def test_sort_by_mhd_without_value():
     mock_db = Mock()
 
     controller = SuppliesController(mock_db)
@@ -43,5 +43,48 @@ def test_sort_by_mhd_with_value():
     mock_db.sort_mhd_desc.assert_not_called()
     mock_db.get_supplies.assert_called_once()
 
+def test_filter_supplies_entry_without_value():
+    mock_db = Mock()
+    mock_db.get_supplies.return_value = ["Pesto", "Kekse"]
+
+    controller = SuppliesController(mock_db)
+    result = controller.filter_supplies_by_entry("")
+
+    mock_db.get_supplies.assert_called_once_with()
+    mock_db.sort_supplies.assert_not_called()
+    assert result == ["Pesto", "Kekse"]
+
+def test_filter_supplies_entry_with_value():
+    mock_db = Mock()
+    mock_db.sort_supplies.return_value = "Pesto"
+
+    controller = SuppliesController(mock_db)
+    result = controller.filter_supplies_by_entry("Pesto")
+
+    mock_db.get_supplies.assert_not_called()
+    mock_db.sort_supplies.assert_called_once_with("Pesto")
+    assert result == "Pesto"
+
+def test_filter_supplies_entry_with_wildcard():
+    mock_db = Mock()
+    mock_db.sort_supplies.return_value = "%Pesto%"
+
+    controller = SuppliesController(mock_db)
+    result = controller.filter_supplies_by_entry("*Pesto*")
+
+    mock_db.get_supplies.assert_not_called()
+    mock_db.sort_supplies.assert_called_once_with("%Pesto%")
+    assert result == "%Pesto%"
+
+def test_filter_supplies_entry_with_many_wildcards():
+    mock_db = Mock()
+    mock_db.sort_supplies.return_value = "%Pes%t%o%"
+
+    controller = SuppliesController(mock_db)
+    result = controller.filter_supplies_by_entry("*Pes*t*o*")
+
+    mock_db.get_supplies.assert_not_called()
+    mock_db.sort_supplies.assert_called_once_with("%Pes%t%o%")
+    assert result == "%Pes%t%o%"
 
 
